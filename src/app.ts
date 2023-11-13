2233a33dc2aadca36ee50083f810c13b34fc27cc
ramiro elisei <ramiroelisei@gmail.com>
2023-11-13 18:22:05 +0100

    Add app to api rest

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import chalk from "chalk";
+
+const app = express();
+
+const port = process.env.PORT ?? 4000;
+
+app.listen(+port, () => {
+  console.log(chalk.green`Listening on http://localhost:${port}`);
+});
+
+export default app;
