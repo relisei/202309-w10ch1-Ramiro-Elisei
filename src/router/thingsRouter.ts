b5e79c96b67e85f57b6082dcc19969649b8b88b6
ramiro elisei <ramiroelisei@gmail.com>
2023-11-14 07:38:20 +0100

    Add thing I already now

--- src/router/thingsRouter.ts
+++ src/router/thingsRouter.ts
@@ -8,4 +8,16 @@ thingsRouter.get("/", (_req, res) => {
   res.status(200).json({ things });
 });
 
+thingsRouter.get("/:thingId", (req, res) => {
+  const thingId = parseInt(req.params.thingId, 10);
+  const thing = things.find((thing) => thing.id === thingId);
+
+  if (!thing) {
+    res.status(404).json({ error: "This thing doesn't exist" });
+    return;
+  }
+
+  res.status(200).json(thing);
+});
+
 export default thingsRouter;
