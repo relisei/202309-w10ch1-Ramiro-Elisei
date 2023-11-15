4db97fcbe1adc3bc72d9b3e361769bb98b19ff0e
ramiro elisei <ramiroelisei@gmail.com>
2023-11-15 14:02:42 +0100

    Add underscore to req paramater in PingController

--- src/features/ping/controller/PingController.ts
+++ src/features/ping/controller/PingController.ts
@@ -1,7 +1,7 @@
 import { type Request, type Response } from "express";
 
 class PingController {
-  getPong(req: Request, res: Response) {
+  getPong(_req: Request, res: Response) {
     res.status(200).json({ message: "🏓" });
   }
 }
